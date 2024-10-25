d4055b82a6a815f076fee55475b515206fb14957
J. Sierra <menelcar_0@hotmail.com>
2024-10-25 22:18:27 +0200

    Update all

---
+++ DBM-Countdown-Pack-Team-Fortress-2-Administrator/CountLoadList.lua
@@ -0,0 +1,6 @@
+local inserted = false
+function DBMCPTF2Administrator()
+	if inserted then return end
+	tinsert(DBM.Counts, {	text	= "T2 Administrator",	value 	= "Administrator", path = "Interface\\AddOns\\DBM-Countdown-Pack-Team-Fortress-2-Administrator\\Administrator\\", max = 4})
+	inserted = true
+end
