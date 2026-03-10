3202fd4f992e9fec13118dc6d3f6562edfd1a3b9
J. Sierra <github@alniarez.de>
2026-03-10 14:18:42 +0100

    update midnight

--- DBM-Countdown-Pack-Mario-Kart/CountLoadList.lua
+++
@@ -1,6 +0,0 @@
-local inserted = false
-function DBMCPMarioKart()
-	if inserted then return end
-	tinsert(DBM.Counts, {	text	= "Mario Kart",	value 	= "MarioKart", path = "Interface\\AddOns\\DBM-Countdown-Pack-Mario-Kart\\MarioKart\\", max = 4})
-	inserted = true
-end
--- DBM-Countdown-Pack-Team-Fortress-2-Administrator/CountLoadList.lua
+++
@@ -1,6 +0,0 @@
-local inserted = false
-function DBMCPTF2Administrator()
-	if inserted then return end
-	tinsert(DBM.Counts, {	text	= "TF2 Administrator",	value 	= "Administrator", path = "Interface\\AddOns\\DBM-Countdown-Pack-Team-Fortress-2-Administrator\\Administrator\\", max = 4})
-	inserted = true
-end
